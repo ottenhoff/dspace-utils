208ffc1a0b7184d976689e96aa3a506a1acfd23b
Sam Ottenhoff <ottenhoff@longsight.com>
2012-07-11 16:10:38 -0400

    export files as part of CSV

--- export-collection-metadata-to-csv.php
+++ export-collection-metadata-to-csv.php
@@ -2,7 +2,7 @@
 <?php
 
 if (count($argv) !== 4) {
-  exit("Bad number of arguments. Please supply database string, collection id, destination CSV\n\n");
+  exit("Bad number of arguments. Please supply database string, collection id, DSpace asset path, destination path\n\n");
 }
 
 
@@ -18,20 +18,19 @@ if (!$conn) {
 
 $fp = fopen($dest_csv, 'w');
 
-$collections = "706";
-
 $sql = "select mv.item_id, mf.element, mf.qualifier, mv.text_value, c.name AS collection_name
 from metadatavalue mv
 inner join metadatafieldregistry mf ON mv.metadata_field_id=mf.metadata_field_id
 inner join item i ON mv.item_id=i.item_id
 inner join collection2item c2i ON i.item_id=c2i.item_id
 inner join collection c ON c2i.collection_id=c.collection_id
-WHERE c2i.collection_id IN ($collections) AND mv.text_value != ''
+WHERE c2i.collection_id=$coll_id AND mv.text_value != ''
 ORDER BY mv.item_id, mv.place";
 
 $res = pg_query ($conn, $sql);
 
-$metadata = array('collection' => 'collection');
+// set the first rows to files and collections
+$metadata = array('files' => 'files', 'collection' => 'collection');
 $out = array();
 
 while ($row = pg_fetch_object ($res)) {
@@ -60,8 +59,27 @@ fputcsv ($fp, $metadata);
 foreach ($out AS $item_id => $arr) {
   $ret = array();
 
+  // find all files for item
+  $sql = "select b.name from bitstream b
+          inner join bundle2bitstream b2b ON b.bitstream_id=b2b.bitstream_id
+          inner join item2bundle i2b ON b2b.bundle_id=i2b.bundle_id
+          inner join item i ON i2b.item_id=i.item_id
+          WHERE i.item_id=$item_id AND b.source NOT LIKE 'Written by %' AND b.source NOT LIKE 'org.dspace.license.%'";
+
+  $res = pg_query ($conn, $sql);
+
+  $files = array();
+  while ($row = pg_fetch_object ($res)) {
+    $files[] = $row->name;
+  }
+
   foreach ($metadata AS $dc) {
-    $ret[] = @$arr[$dc];
+    if ($dc == "files") {
+      $ret[] = implode ("|", $files);
+    }
+    else {
+      $ret[] = @$arr[$dc];
+    }
   }
 
   fputcsv ($fp, $ret);
